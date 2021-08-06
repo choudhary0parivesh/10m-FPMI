2cda6b9b712838b6bd55e454b6ee5638fed2114e
Parivesh Choudhary <choudhary0parivesh@gmail.com>
2021-08-07 00:23:03 +0530

    add comments

--- Fabry Perot Cavity/HOM_analytical_sol.py
+++ Fabry Perot Cavity/HOM_analytical_sol.py
@@ -21,16 +21,11 @@ print("F",F)
   
 fig, ax = plt.subplots(1,1)
   
-#Z = 1/(1+((2*F/np.pi)**2)*np.sin(X*np.arccos((1-L/Y)**0.5))**2)**0.5
-#Z=  1/(1+((2*F/np.pi)**2)*np.sin(X*np.arccos((1-L/Y)**0.5))**2)**0.5
-
-
 #relative to fun HOM
 
 Z= 1/((1+((2*F/np.pi)**2)*(np.sin(X*np.arccos((1-L/Y)**0.5)))**2)**0.5)  
 
 # plots filled contour plot
-#cf = ax.contourf(X, Y, Z,locator=ticker.LogLocator(),cmap=cm.PuBu_r)
   
 ax.set_title('RoC vs TEM')
 ax.set_xlabel('TEM (m)')
@@ -38,9 +33,6 @@ ax.set_ylabel('RoC ')
 ax.set_xticks([1,2,3,4,5,6,7,8,9,10])
 ax.axhline(y = 31.6, color = 'r', linestyle = 'dashed')
 ax.axhline(y = 34, color = 'b', linestyle = 'dashed')    
-#plt.clim(0,1)
-#fig.colorbar(cf, ax=ax)  
-#plt.figure(figsize=(1,1))
 
 #plt.show()
 im = plt.imshow(Z, extent=[1, 10, 24, 36], origin='lower',cmap=cm.rainbow, norm=colors.LogNorm(), alpha=0.5)
